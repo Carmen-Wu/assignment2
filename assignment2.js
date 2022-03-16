3e41366ae048d215825bf27d0ac03108a46d4a8e
orzdian <98924155+orzdian@users.noreply.github.com>
2022-03-15 22:34:59 -0400

    recover accident delect

--- assignment2.js
+++ assignment2.js
@@ -13,7 +13,7 @@ Array.prototype.myEach = function (callbackFn) {
 // MAP //
 Array.prototype.myMap = function (callbackFn) {
     const newArray = [this.length];
-    for (let i = 0; i < this.length; i++) { 
+    for (let i = 0; i < this.length; i++) {
       if (this[i] === undefined) {
         continue;}
       const result = callbackFn(this[i], i, this);
@@ -77,3 +77,69 @@ Array.prototype.mySome = function(callbackFn) {
     }
     return false;
   };
+  
+ // myIndex//
+ Array.prototype.myIndexOf = function(searchElement, fromIndex) {
+  if(fromIndex=== undefined){
+    fromIndex = 0; //initialze fromIndex if it is not given to 0
+  }
+  if(fromIndex< 0){
+    fromIndex = this.length + fromIndex;
+  } 
+  for (let i = fromIndex; i < this.length; i++) { //scanning through element from starting element
+    if(this[i] === searchElement){ // if element matched 
+    return i;// return position of index
+    }
+  }
+  return -1;// not found
+};
+
+//myPush//
+Array.prototype.myPush = function(...args) {  // Use rest parameter to accept any number of input arguments
+  let args_index = 0;  // Index of new element
+  let length = this.length;   // Length of "this" array
+  // The last element of "this" array is at length - 1
+  for (let i = length; i < length + args.length; i++) {
+      this[i] = args[args_index];  // Add new element to end of "this" array
+      args_index++;
+  }
+  return this.length;  // Return new length of "this" array
+};
+
+//myLastIndexOf//
+Array.prototype.myLastIndexOf = function(searchElement, fromIndex) {
+if(fromIndex=== undefined){
+  fromIndex = this.length; //initialze fromIndex from the lenth of array
+}
+if(fromIndex< 0){
+  fromIndex = this.length + fromIndex;
+} 
+for (let i = fromIndex; i >= 0; i--) { //scan from the last index to the first index
+  if(this[i] === searchElement){// if matched
+  return i; // return position of index
+  }
+}
+return -1;// not found
+};
+
+//myKeys//
+Object.myKeys = function(obj) {
+let obj_name = [obj.size]; 
+let i = 0; // initialize index to 0
+for (var name in obj){ // loop all enumerable property name
+  obj_name[i] =name; // input enumerable property name into this index
+  i++; // increment index
+}
+return obj_name; //return enumerable property names
+}
+
+//myValues//
+Object.myValues = function(obj) {
+let obj_value = [obj.size];
+let i = 0; // initialize index to 0
+for (var name in obj){ // loop all enumerable property name
+  obj_value[i] = obj[name]; // input enumerable property value into this index
+  i++; // increment index
+}
+return obj_value;//return enumerable property value
+};
